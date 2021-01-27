9a1250db4e6ebab381c26a4ca0c63efccbe5c3ac
Ivqonnada Al Mufarrih <ivqonnada@gmail.com>
2021-01-27 16:51:01 +0700

    Change student dummy default email

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -72,7 +72,7 @@ class UserSeeder extends Seeder
         $users[] = [
             'first_name' => 'Ivqonnada ',
             'last_name' => 'Al Mufarrih',
-            'email' => 'ivqonnada@gmail.com',
+            'email' => 'student@gmail.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
